fa0d01786767b75ae3a271e1370c31369fbb7f2f
Арсений Серов <aaserov_1@miem.hse.ru>
2023-09-14 20:42:43 +0300

    First test commit

---
+++ src/test/java/ru/serov/springchat/WebChat/WebChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.serov.springchat.WebChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
